5a213766eb9167b40f0d6b22c93f952b72f41997
Goodernews <tbogoodnews@gmail.com>
2021-03-04 16:09:00 -0800

    Updated

--- Lab/Lab5.5/Quinn_Taylor_Problem1.asm
+++ Lab/Lab5.5/Quinn_Taylor_Problem1.asm
@@ -1,10 +1,9 @@
          .data
+         str:  .word  20
          .align    2
          .text
 
          .globl  main
-
-
 
 #Create a function isLowerCase(char c) that is passed a character in $a0 
 #and returns, in $v0, a 1 if the character is considered a lowercase 
@@ -13,9 +12,25 @@ $value in ASCII (i.e. 'a' is decimal value 97 in ASCII and 'z' is 122)
 #previous lab.]
 
 isLowerCase:
+         li    $v0, 1
+         b     is_lower_greater
+
+is_lower_greater: #met first 
+         addi  $t1, $t1, -97 
+         bgezal $t1, is_lower_less #meets lower bound ascii
+         b not_lowercase
+
+is_lower_less:
+         addi  $t1, $t0, -25
+         bgezal   $t1, exit_lower_case #meets lower bound ascii
+         b     not_lowercase
          
+not_lowercase:
+         li    $v0, 0
+         b     exit_lower_case
 
-
+exit_lower_case:
+         jr    $ra
 
 #b) Create a function getUpperCase(char c) that is passed a character, 
 #in $a0, that is a lowercase value in ASCII and returns, in $v0, the 
@@ -25,8 +40,24 @@ isLowerCase:
 #[Pro tip: You already basically did this for the last lab]
 
 getUpperCase:
+         move  $t0, $a0 # load to temporary value
+         li    $t1, 0 #default return value
+         b     is_greater
 
+is_greater: #met first 
+         addi  $t0, $t0, -97 
+         bgezal $t0, is_less #meets lower bound ascii
+         b exit
 
+is_less:
+         addi  $t0, $t0, -25
+         bgezal $t0, exit #meets lower bound ascii
+         addi  $t1, $v0, -32
+         b exit
+         
+exit:
+         move  $v0, $t1
+         jr    $ra
 
 #c) Create a function called convertString that takes the address of a 
 #string as an argument in $a0. That function should go through all 
@@ -38,7 +69,27 @@ getUpperCase:
 #both to create more readable code.)
 
 convertString:
+         lb    $sp, $a0
+         #intialize
+         #load first character
+         b     recurse_convert
          
+recurse_convert:
+         #trigger base case
+         j     isLowerCase
+         bgtz  $a0,  do_the_thing
+         #     shift to next character
+         b     recurse_convert
+
+do_the_thing: #zhu li
+         j     getUpperCase
+         #store byte back
+         #     shift to next character
+         b     recurse_convert
+
+
+convert_string_break: #if base case reached (no more letters!)
+         jr    $ra
 
 # d) In the main function, create a global string called str that 
 # initially has space for 20 characters (plus the null terminator). 
@@ -49,4 +100,14 @@ convertString:
 
 
 main:
-         
+         li    $v0, 5
+         syscall
+         lw    str, $v0
+         la    $a0, str 
+
+         j     convertString
+
+         #load fixed text into $a0
+         li    $v0, 1
+         syscall
+         jr    $ra
