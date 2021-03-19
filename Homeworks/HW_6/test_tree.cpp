021776f3a21b986c6f3295acf06c28a73998e336
Goodernews <tbogoodnews@gmail.com>
2021-03-19 10:23:04 -0700

    created test file

--- Homeworks/HW_6/test_tree.cpp
+++ Homeworks/HW_6/test_tree.cpp
@@ -1,8 +1,29 @@
 #include <iostream>
-#include "tree.hh"
+#include "tree.cpp"
 
 using namespace std;
 
 int main(){
+	cout << "Building Root of tree" <<endl;
+	tree_ptr_t test_tree = create_tree(1,1);
+	cout << "Root value is: " << test_tree.value<<endl;
+		
+	cout << "\nAdding two children and grand children" <<endl;
+	test_tree.left = create_tree(12,5, create_tree(85,9), create_tree(85, 2)); 
+	test_tree.right = create_tree(100, 9, create_tree(101, 3), create_tree(4, 500));
+	cout << "Left node value: " << test_tree.left.value << endl;
+	cout << "Right node value: " << test_tree.right.value << endl;
 
+	cout << "\nPath to first 5 value: " << path_to(test_tree, 5) << endl;
+	cout << "Path to node with value 2: " << path_to(test_tree, 2) << endl;
+
+	cout << "\nNode at LR: " << node_at(test_tree, "LR") << endl;
+	cout << "Node at RR: " << node_at(test_tree, "RR") << endl;
+	cout << "Node at R: " << node_at(test_tree, "R") << endl;
+
+	cout << "\nDeleting tree" <<endl;
+	destroy_tree(test_tree);
+
+	cout << "\nTasks completed Succesfully!" <<endl;
+	return 0;
 }
