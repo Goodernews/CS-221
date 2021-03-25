0278d0e3c4cf4793871be511693a75461b791d9b
Goodernews <tbogoodnews@gmail.com>
2021-03-24 17:23:20 -0700

    Rough attempt

--- Homeworks/HW_7/htree.cc
+++ Homeworks/HW_7/htree.cc
@@ -10,27 +10,41 @@ using namespace std;
 
 // Return an optional list of directions from root to a node of a given key.
 // If key not contained in this tree, returns nullptr
-HTree::possible_path_t
-HTree::path_to(key_t key) const
+
+
+HTree::key_t HTree::get_key() const {
+	return key_;
+}
+
+HTree::value_t HTree::get_value() const {
+	return value_;
+}
+
+
+HTree::tree_ptr_t HTree::get_child(HTree::Direction dir){
+	if (dir == Direction::LEFT){
+	return left_;
+	}
+       if (dir == Direction::RIGHT){
+	return right_;
+	}	       
+}
+
+HTree::possible_path HTree::path_to(key_t key) const
 {
   return nullptr;
 }
 
-key_t HTree::get_key(){
-}
 
-tree_ptr_t Htree::get_child(){
-
-}
-
-tree_ptr_t Htree::get_child(Direction dir){
-	 
-}
-
-value_t HTree::get_value(){
-
-}
-
+/*
 void HTree::destroy_tree(){
-
+	if (left_!=nullptr){
+		destroy_tree(left_);
+	}
+	if (right_!=nullptr){
+		destroy_tree(right_);
+	}
+	delete parent;
 }
+
+*/
