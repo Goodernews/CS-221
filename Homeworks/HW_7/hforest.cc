8d9c25c16ec2221f99b0df48a404bcbe1c91f191
deeptronos <ecnemec1132@gmail.com>
2021-03-24 21:02:54 -0700

    Began implementing HForest stuff
    Implemented HForest and most of its required methods (except for pop_tree()), with HForest storing its collection as a vector. I'm now going to re-write HForest to be a list, so that I can implement pop_tree() efficiently

--- Homeworks/HW_7/hforest.cc
+++ Homeworks/HW_7/hforest.cc
@@ -1,13 +1,43 @@
-//
-// Created by Cole Nemec on 3/22/21.
-//
+#include "hforest.hh"
 
-size_t HForest::size(forest_ptr_t Forest){
 
+// Implemented this functionality in the applicable functions (add_tree(), pop_tree()) in hforest.hh
+//void makeHForestHeap(HForest::forest_ptr_t forest){
+//
+//    int vecBegin = *forest.entries.begin();
+//    int vecEnd   = *forest.entries.end();
+//
+//    std::make_heap(vecBegin, vecEnd, compare_trees());  // Make the entries vector a heap
+//}
+
+size_t HForest::size(HForest::forest_ptr_t forest){  // Return how many trees are in the forest
+
+    if(forest == nullptr){  // If forest is a nullptr, we don't wanna do anything else with this function, so we return 0
+        return 0;
+    }
+
+    HForest deRefForest = *forest;  // Dereference forest so that we can access it and its data
+    return deRefForest.entries.size();  // Return the size() of the "entries" vector of the HForest
 }
 
-forest_ptr_t HForest::add_tree(HTree::tree_ptr_t Tree){
+// add_tree() is implemented in hforest.hh
+//void HForest::add_tree(HTree::tree_ptr_t tree){ //  Take a pointer to an HTree (of type HTree::tree_ptr_t) and add it to the forest
+//
+//}
 
+//Plan:
+    // create reference of some sort to HForest.entries[0]
+    // use a vector method to remove the entry from entries
+    // return the reference we created
+
+HTree::tree_ptr_t HForest::pop_tree(HForest::forest_ptr_t forest){    //  Return a pointer to the HTree with the highest value in the root node, and remove it from the forest.
+    if(forest == nullptr){  // If forest is a nullptr, we won't do anything else with this function, so we immediately return nullptr
+        return nullptr;
+    }
+    HForest deRefForest = *forest;
+    // The forest will, presumably, already have its entries in heap order, since add_tree() runs make_heap() after adding a new tree to entries. Thus, the tree with the highest-value root node will be the first entry
+    HTree()
 }
 
-void HForest::pop_tree(forest_ptr_t Forest){}
+
+
--- Homeworks/HW_7/hforest.hh
+++ Homeworks/HW_7/hforest.hh
@@ -1,15 +1,37 @@
-//
-// Created by Cole Nemec on 3/22/21.
-//
+//Store trees in vector, organize vector with make_heap
 
-#ifndef HW_7_HFOREST_HH
-#define HW_7_HFOREST_HH
+#pragma once
+
+#include <list>
+#include <memory>
+#include <vector>
+
+#include "htree.hh"
+
+//Can/should I just put this right in the header file??
+bool compare_trees(HTree::tree_ptr_t t1, HTree::tree_ptr_t t2) {  // Eitan's compare_trees() function
+    return t1->get_value() < t2->get_value();
+}
 
 class HForest{
+    int size;
+    vector<HTree> entries;
 public:
-	using size_t=int;
-	using forest_ptr_t=std::vector;
-};	
+	using size_t = int;
+	using forest_ptr_t = std::shared_ptr<const HForest>;
+
+	//Initialization:
+	HForest(HTree firstForestTree): entries{firstForestTree} {};   // Create a new HForest, with firstForestTree as the first entry in it
+	HForest();  // Default forest: no entries
+	~HForest();
+
+	size_t size(forest_ptr_t forest);  // Return how many trees are in the forest
+    // add_tree() is defined here, inline, since we need to access HForest.entries without passing some sort of reference to HForest when we call the function
+	void add_tree(HTree::tree_ptr_t tree) { entries.push_back(*tree); std::make_heap(entries.begin(), entries.end(), compare_trees()) };  // Take a pointer to an HTree (of type HTree::tree_ptr_t) and add it to the forest
+
+    HTree::tree_ptr_t pop_tree(forest_ptr_t forest); //  Return a pointer to the HTree with the highest value in the root node, and remove it from the forest.
+
+};
 
 
-#endif //HW_7_HFOREST_HH
+
