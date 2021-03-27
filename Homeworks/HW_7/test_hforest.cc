40694664f9e3aadeb8e970ee7c8d3d07804b9990
deeptronos <ecnemec1132@gmail.com>
2021-03-26 18:09:30 -0700

    Update test_hforest.cc

--- Homeworks/HW_7/test_hforest.cc
+++ Homeworks/HW_7/test_hforest.cc
@@ -1,9 +1,11 @@
-#include "htree.hh"
-#include "hforest.hh"
+
 
 #include <iostream>
 #include <stdio.h>
 #include <assert.h>
+
+#include "htree.hh"
+#include "hforest.hh"
 
 int main(){
     return 0;
