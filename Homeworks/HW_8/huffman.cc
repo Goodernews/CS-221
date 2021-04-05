9ff806e4bb3084a6012f2a83e1d0a3d67970cf7b
Goodernews <tbogoodnews@gmail.com>
2021-04-04 20:43:15 -0700

    Update huffman.cc

--- Homeworks/HW_8/huffman.cc
+++ Homeworks/HW_8/huffman.cc
@@ -11,17 +11,33 @@ Huffman::Huffman(){ //because this will start out the same every time, it can be
 	auto forest = HForest(); //create forest
 	
 	for(i=0; i=<ALPHABET_SIZE+1; i++){
-	forest->add_tree(std::make_shared<HTree>(i, 1, nullptr, nullptr));
-	}
+	forest->add_tree(std::make_shared<HTree>(i, 0, nullptr, nullptr));
+	} //makes a blank huffman forest
 
-	
-	//add nodes to forest
-	//RECURSIVE STEP
-	//
+	auto encoder_ = build_tree(forest); //turns forest into an encoding tree
+
+}
+
+
+
+HForest::forest_ptr_t break_tree(HTree::tree_ptr_t){
+       	// splits tree into forest
+	HForest::forest_ptr_t forest = HForest();
+	while(HForest->size()<ALPHABET_SIZE){
+		//find a leaf node
+		//add to forest
+		//remove pointer from parent
+	}
+	return forest;
+}
+
+
+HTree::tree_ptr_t build_tree(HForest::forest_ptr_t forest){
+       	//makes forest into huffman tree
 	while(forest->size>1){ //Pop two trees
 		auto smallest_tree = forest->pop_tree();
 		auto small_tree = forest->pop_tree(); //gets smallest trees
-		HTree::value_t num_nodes = smallest_tree->get_value() + small_tree->get_vale(); //Num children
+		HTree::value_t num_nodes = smallest_tree->get_value() + small_tree->get_value(); //Num children
 		auto merged_tree = make_shared<HTree>(holder_key, //to signify it is not a leaf node
 				num_nodes, //root value temporarily holds number of leaves in tree
 				small_tree, //adds nodes to merging node
@@ -30,20 +46,6 @@ Huffman::Huffman(){ //because this will start out the same every time, it can be
 	}
 
 	return forest->pop_tree();//clear forest add base tree to encoder_
-	
-}
-
-
-
-HForest::forest_ptr_t break_tree(HTree::tree_ptr_t){
-       	// splits tree into forest
-	
-}
-
-
-HTree::tree_ptr_t build_tree(HForest::forest_ptr_t forest){
-       	//makes forest into huffman tree
-
 }
 
 
@@ -51,15 +53,12 @@ HTree::tree_ptr_t build_tree(HForest::forest_ptr_t forest){
 
 bits_t Huffman::encode(int symbol){
 	auto update_path = encoder_->path_to(symbol); //find pointer to node with a key
-	//pointer to value of node
-	
-	//subtract current freq value from all parent nodes
-	
-	//add one to value of node frequency
-	
-	//find node to swap with
-	//swap
-	
+	auto node_pointer = ...; //pointer to value of node
+		
+	//add one to value of target node frequency
+
+	auto broken_tree_ = break_tree(forest); //break apart tree into forest
+
 	//add current value to parent nodes
 	//
 }
