aeee2a88034e1aeb7262106c2dec56d84b2b97ed
Goodernews <tbogoodnews@gmail.com>
2021-04-08 09:59:21 -0700

    Updated

--- Lab/Lab7/highest.cpp
+++ Lab/Lab7/highest.cpp
@@ -3,39 +3,37 @@
 
 const int* highest(int val, const int* start, const int* finish){
 
+	if (start==finish || finish<start ||finish==start++){
+		return nullptr;
+	}
+	
+	//std::cout << finish << std::endl;
+	//std::cout << start << std::endl;
+
+	auto mid_pointer = (finish-start)/2+start; //the pointer in the middle
 
 
-if (start==finish){
+	if (*start == val){
+		return start;
+	}
+	//std::cout << mid_pointer << std::endl;
+
+	if (mid_pointer == nullptr){
+		return nullptr;
+	}
+
+	if (val == *mid_pointer){
+		return mid_pointer;
+	}
+
+	if (val >*mid_pointer){ //The value is greater that the mid point
+		return highest(val, mid_pointer, finish); // set start to the mid point
+	}
+
+	else if (val<*mid_pointer){ // value less than the mid_pointer
+		return highest(val, start, mid_pointer);
+	}
 	return nullptr;
-}
-
-if (val==*(finish-1)){
-	return finish;
-}
-
-if (val==*start){
-	return start;
-}
-
-
-if (val<*start || val>*finish){
-	return nullptr;
-}
-
-const int *mid_pointer = (finish-start)/2+start; //the pointer in the middle
-
-
-if (val >*mid_pointer){ //The value is greater that the mid point
-	return highest(val, mid_pointer, finish); // set start to the mid point
-
-}
-
-else if (val<*mid_pointer){ // value less than the mid_pointer
-	return highest(val, start, mid_pointer);
-}
-
-
-return nullptr;
 }
 
 
