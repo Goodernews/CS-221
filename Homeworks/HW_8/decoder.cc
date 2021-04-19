2ec92cb7817b679d35ffa4c4381a6bbd97b540b2
Alan <alanrjes@gmail.com>
2021-04-19 14:18:15 -0700

    Partial debugged
    Temporarily removed huffman calls and debugged file reading/writing (huffman calls now restored)

--- Homeworks/HW_8/decoder.cc
+++ Homeworks/HW_8/decoder.cc
@@ -3,23 +3,23 @@
 #include <iostream>
 #include <fstream>
 #include <sstream>
-#include <string>
+#include <cstring>
 
-// this is not debugged, just a draft of what "should" work
+// this is not fully debugged, just a draft of what "should" work
 
 int main(int argc, char* argv[]) {
-  while (i=1; i<argc; i++) {  // start at one to skip the first argument (run file name), and loop for every file
-    ifstream ifile(&filename);  // file stream to read binary encoded text from
-    ofstream ofile(&filename + ".plaintext");  // file stream to write decoded text to
+  for (int i=1; i<argc; i++) {
+    char* filename = argv[i];  // start at one to skip the first argument (run file name), and loop for every file
+    std::ifstream ifile(filename);  // file stream to read binary encoded text from
+    std::ofstream ofile(strcat(filename, ".plaintext"));  // file stream to write decoded text to
 
     if (ifile.is_open()) {  // is a valid readable file
-      BitIO::BitIO bitio(nullptr, &ifile);
-      Huffman::Huffman huff;
+      BitIO bitio(nullptr, &ifile);
+      Huffman huff;
       char byte;
 
-      while (byte != Huffman::HEO) {
+      while (byte != Huffman::HEOF) {
         byte = char(huff.decode(bitio.input_bit()));  // huffman tree does its huffmagic, gives decoded byte
-        ofile.put(byte);
       }
     }
   }
--- Homeworks/HW_8/encoder.cc
+++ Homeworks/HW_8/encoder.cc
@@ -3,22 +3,24 @@
 #include <iostream>
 #include <fstream>
 #include <sstream>
-#include <string>
+#include <cstring>
 
-// this is not debugged, just a draft of what "should" work
+// this is not fully debugged, just a draft of what "should" work
 
 int main(int argc, char* argv[]) {
-  while (i=1; i<argc; i++) {  // start at one to skip the first argument (run file name), and loop for every file
-    ifstream ifile(&filename);  // file stream to read plain text for encoding from
-    ofstream ofile(&filename + ".comp");  // file stream to write encoded version to
+  for (int i=1; i<argc; i++) {
+    char* filename = argv[i];  // start at one to skip the first argument (run file name), and loop for every file
+    std::ifstream ifile(filename);  // file stream to read plain text for encoding from
+    std::ofstream ofile(strcat(filename, ".comp"));  // file stream to write encoded version to
 
     if (ifile.is_open()) {  // is a valid readable file
-      BitIO::BitIO bitio(&ofile, nullptr);
-      Huffman::Huffman huff;
+      BitIO bitio(&ofile, nullptr);
+      Huffman huff;
+      char byte;
 
       while (ifile.get(byte)) {
-        enbyte = huff.encode(byte)  // huffman tree does its huffmagic, gives encoded byte
-        while (i=0; i<8; i++) {  // for each bit in the encoded byte
+        std::vector<bool> enbyte = huff.encode(byte);  // huffman tree does its huffmagic, gives encoded byte
+        for (i=0; i<8; i++) {  // for each bit in the encoded byte
           bool bit = enbyte[i];
           bitio.output_bit(bit);
         }
